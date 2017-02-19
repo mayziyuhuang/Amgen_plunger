9c88479a69395dd3eee2b877d492df21c23e31ce
Zi-Yu Huang <zhhuang@caltech.edu>
2017-02-18 17:23:48 -0800

    slice off time less than 0

--- flow_data.py
+++ flow_data.py
@@ -9,9 +9,11 @@ rc={'lines.linewidth': 2, 'axes.labelsize': 18, 'axes.titlesize': 18}
 sns.set(rc=rc)
 
 # Read in flow rate data files with Pandas
-flow_data = pd.read_csv('flow_20170217_3ml_70mm-min_0um_2nd.csv')
+flow_data = pd.read_csv('20170217data/flow_20170217_3ml_70mm-min_0um_2nd.csv')
 flow_data.columns = ['sample', 'time', 'flow']
 
+# The flow sensor can sense the flow in two direction
+# Make the flow rate to be positive
 flow_data.loc[:,'flow'] *= -1
 
 plt.plot(flow_data['time'], flow_data['flow'], marker='.',
--- force_travel.py
+++ force_travel.py
@@ -9,12 +9,31 @@ rc={'lines.linewidth': 2, 'axes.labelsize': 18, 'axes.titlesize': 18}
 sns.set(rc=rc)
 
 # Read in flow rate data files with Pandas
-force_data = pd.read_csv('force_travel_20170217_3ml_70mm-min_0um_2nd.csv')
+force_data = pd.read_csv('20170217data/force_travel_20170217_3ml_70mm-min_0um_2nd.csv')
 force_data.columns = ['reading', 'load', 'travel', 'time']
 
+# The test stand pushes the syring down so the travel indicator is negative
+# Make the displacement to be positive
 force_data.loc[:,'travel'] *= -1
 
-plt.plot(force_data['travel'], force_data['load'], marker='.',
+
+# Slice off the time < 0
+# When the travel is not zero anymore, consider it starts moving
+# Set the one data point ahead of it as time zero
+
+# Find the index that need to be cut off
+force_time_less0 = force_data[force_data['travel'] <= 0]
+length = len(force_time_less0.index)
+# Define force_time0 dataframe
+force_time0 = force_data.iloc[length-1:]
+
+
+# Find the index of the row with largest load value
+max_force_index = force_time0['load'].idxmax()
+# Find the maximum force
+max_force = force_time0.loc[max_force_index, 'load']
+
+plt.plot(force_time0['travel'], force_time0['load'], marker='.',
          linestyle='-')
 plt.xlabel('travel distance (mm)')
 plt.ylabel('load (N)')
